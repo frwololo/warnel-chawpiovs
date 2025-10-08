6d8d1409f2fea760f8bccea8d22d1eed0211b5df
frwololo <wagic.the.homebrew@gmail.com>
2025-10-08 05:26:33 +0000

    git-svn-id: file:///cygdrive/j/svnrepository/trunk/wc@73 26b7b23b-8271-de4e-bc0c-b195ce3ddbb7

---
+++ src/wc/CardBack.gd
@@ -0,0 +1,39 @@
+extends CardBackGlow
+
+var font_scaled : float = 1
+onready var art := $Art
+
+func _ready() -> void:
+	viewed_node = $Viewed
+	
+# Since we have a label on top of our image, we use this to scale the label
+# as well
+# We don't care about the viewed icon, since it will never be visible
+# in the viewport focus.
+func scale_to(scale_multiplier: float) -> void:	
+	if font_scaled != scale_multiplier:
+		var label : Label = $Label
+		label.rect_min_size *= scale_multiplier
+		# We need to adjust the Viewed Container
+		# a bit more to make the text land in the middle
+#		$"VBoxContainer/CenterContainer".rect_min_size *= scale_multiplier * 1.5
+		var label_font : Font = label.get("custom_fonts/font").duplicate()
+		label_font.size *= scale_multiplier
+		label.set("custom_fonts/font", label_font)
+		font_scaled = scale_multiplier
+	
+func set_card_art(filename) -> void:
+	var new_img = WCUtils.load_img(filename)
+	if not new_img:
+		return
+	var imgtex = ImageTexture.new()
+	imgtex.create_from_image(new_img)	
+	art.texture = imgtex
+	art.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+	# In case the generic art has been modulated, we switch it back to normal colour
+	art.self_modulate = Color(1,1,1)
+	
+	#since we're setting a specific cart, we disable the default stuff
+	$TextureRect2.visible = false
+	$Label.visible = false
+	stop_card_back_animation()
---
+++ src/wc/ScriptingEngine/Scripts/SimplifiedStackScript.gd
@@ -0,0 +1,46 @@
+class_name SimplifiedStackScript
+extends Node
+
+var task_name
+var task
+
+func _init(_name, _task):
+	task_name = _name
+	task = _task
+	#hard modify the name here because at least for damaged received I'm passing "undefedn" as the name
+	# maybe that needs to be modifed there instead of here
+	task.script_name = task_name
+	
+	#enforce some values for filter matching
+	if (task.subjects):
+		task.script_definition["target"] = task.subjects[0]
+
+func execute():
+	var sceng = cfc.scripting_engine.new([task], task.owner,task.trigger_object, task.trigger_details)
+	var retcode = sceng.call(task_name, task)
+
+func get_tasks() -> Array:
+	return [task]
+
+func get_script_by_event_name(_name):
+	if task_name == _name:
+		return task
+	return null
+	
+func added_to_global_stack():
+	#scripting_bus.emit_signal("before_" + get_event_name())	
+	return	
+
+func matches_filters(script, filters:Dictionary, owner_card):
+	var owner_hero_id = owner_card.get_owner_hero_id()
+	if (owner_hero_id > 0):
+		for v in ["my_hero"]:
+			#TODO move to const
+			WCUtils.search_and_replace(filters, v, gameData.get_hero_card(owner_hero_id), true)
+
+	if (filters):
+		var tmp = 0	
+	var script_details = script.script_definition
+	var result = WCUtils.is_element1_in_element2(filters, script_details)
+
+	return result
